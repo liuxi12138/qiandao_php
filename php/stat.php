abdb0bd85fa8dfd7ae2ce02d7e100929c30a7b62
edward <lx_einstein@sina.com>
2016-02-02 22:10:19 +0800

    stat early

--- php/stat.php
+++ php/stat.php
@@ -21,6 +21,7 @@ $edate="2016-2-3";
 	echo "<td>值班总次数</td>";
 	echo "<td>未签退</td>";
 	echo "<td>按要求在岗</td>";
+	echo "<td>早退次数</td>";
 	echo "<td>额外值班次数</td>";
 	echo "<td>缺勤时间</td>";
 	echo "</tr>";
@@ -39,19 +40,18 @@ while($user_array=mysqli_fetch_array($user_query))
 		$over_query=mysqli_query($con,$over_sql);
 		$over_array=mysqli_fetch_array($over_query);
 	echo "<td>".$over_array['count(*)']."</td>";//未签退
-	// $classid=$array['classid'];
-	// $num_sql="select count(*) from dutys where `dutys`.`classid`='$classid' and date >= '$sdate' and date <= '$edate';";
-	// $num_query=mysqli_query($con,$num_sql) or die("1");
-	// $num_array=mysqli_fetch_array($num_query);
+		$zaigang_sql="select count(*) from user_dutys,dutys where `user_dutys`.`classid`='$classid' and `user_dutys`.`onweek`=`dutys`.`week` and `user_dutys`.`ontime`=`dutys`.`ontime` and `dutys`.`over`='1' and date >= '$sdate' and date <= '$edate';";
+		$zaigang_query=mysqli_query($con,$zaigang_sql);
+		$zaigang=mysqli_fetch_array($zaigang_query);
+		// var_dump($zaigang);
+	echo "<td>".$zaigang["count(*)"]."</td>";//按要求在岗
+		$early_sql="select count(*) from dutys where classid='$classid' and early=1 and over=1";
+		$early_query=mysqli_query($con,$early_sql);
+		$early=mysqli_fetch_array($early_query);
+	echo "<td>".$early["count(*)"]."</td>";//早退次数
+	echo "<td>".$user_array['name']."</td>";//额外值班次数
 
-	$zaigang_sql="select count(*) from user_dutys,($sql) as zaigang where `zaigang`.`over`=0 and `user_dutys`.`onweek`=`zaigang`.`week` and `user_dutys`.`ontime`=`zaigang`.`ontime`";
-	// $zaigang_sql="select count(*) from user_dutys,dutys where `user_dutys`.`classid`='$classid' and `user_dutys`.`onweek`=`dutys`.`week` and `user_dutys`.`ontime`=`dutys`.`ontime` and `dutys`.`over`='1';";
-	$zaigang_query=mysqli_query($con,$sql);
-	$zaigang=mysqli_fetch_array($zaigang_query);
-	var_dump($zaigang);
-	echo "<td>".$zaigang["count(*)"]."</td>";
-	echo "<td>".$user_array['name']."</td>";
-	echo "<td>".$user_array['name']."</td>";
+	echo "<td>".$user_array['name']."</td>";//缺勤时间
 	echo "</tr>";
 	
 }
