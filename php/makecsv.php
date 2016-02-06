a5c1f7141f9a94fa3e3d721bdc8f06d07e2efeb7
edward <lx_einstein@sina.com>
2016-02-06 13:14:20 +0800

    daochu muban

---
+++ php/makecsv.php
@@ -0,0 +1,17 @@
+<?php
+if (!empty($_POST['muban'])){
+	$list = array("学号&姓名&部门&生日&(正式/试用)&值班时间","13110402096&刘熹&技术部&1994-4-2&正式&1|12,5|34");
+	// var_dump($list);
+	$fp = fopen('C:\file.csv', 'w');
+	foreach ($list as $line) {
+		$csvline=explode('&', $line);
+		// var_dump($csvline);
+		fputcsv($fp,$csvline);
+	}
+	fclose($fp);
+	$data['muban']="success";
+	echo json_encode($data);
+}else{
+	$data['muban']="fail";
+	echo json_encode($data);
+}
